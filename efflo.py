7ceb98e2ac69c2ad47f49dfdff3f07e860885587
RedstoneScratch <31410740+redstone-scratch@users.noreply.github.com>
2023-01-21 07:34:50 -0500

    Please fix this (its broken)

--- efflo.py
+++ efflo.py
@@ -2,6 +2,7 @@ import sys
 
 code = None
 stack = []
+procedures = []
 
 with open(sys.argv[1], 'rt', encoding='utf8') as file:
     code = file.read()
@@ -23,13 +24,8 @@ def _repeat_all(func):
 code = code.split(';')
 code = list(reversed(code))
 
-while code:
-    line = code.pop().lstrip()
-    if line.startswith('*') and line.endswith('*'): continue
-    exec_ = line.split(' ')
-    if exec_[0] == '//':
-        continue
-    elif exec_[0] == 'ADD':
+def do_code(exec_):
+    if exec_[0] == 'PUSH':
         _add(exec_[1])
     elif exec_[0] == 'POP':
         stack.pop()
@@ -37,13 +33,52 @@ while code:
         _reverse()
     elif exec_[0] == 'PRINT_ASCII':
         _print()
+    elif exec_[0] == 'PRINT_NUM':
+        print(stack.pop())
     elif exec_[0] == 'REPEAT_ALL':
         while stack:
-            if exec_[1] == 'ADD':
-                _add(stack.pop())
-            elif exec_[1] == 'REVERSE':
-                _reverse()
-            elif exec_[1] == 'PRINT_ASCII':
-                _print()
+            if str(exec_[1]).startswith('$'):
+                i = procedures.index(str(exec_[1][1:]))
+                for instruction in procedures[i + 1]:
+                    do_code(instruction.upper())
+            else:
+                do_code(exec_[1].upper())
+    elif exec_[0] == 'PROC':
+        procedures.append(exec_[1], exec_[2:])
+    elif exec_[0] == '+':
+        # first item + second item
+        stack.append \
+            (float(stack.pop()) + float(stack.pop()))
+    elif exec_[0] == '-':
+        # first item - second item
+        stack.append \
+            (float(stack.pop()) - float(stack.pop()))
+    elif exec_[0] == '*':
+        # first item * second item
+        stack.append \
+            (float(stack.pop()) * float(stack.pop()))
+    elif exec_[0] == '/':
+        # first item / second item
+        stack.append \
+            (float(stack.pop()) / float(stack.pop()))
+    elif exec_[0] == '%':
+        # first item % second item
+        stack.append \
+            (float(stack.pop()) % float(stack.pop()))
+    elif exec_[0] == 'STRING':
+        for char in str(exec_[1])[0:]:
+            if char == '_':
+                stack.append(ord(' '))
+            else:
+                stack.append(ord(char))
     else:
-        print('UNKNOWN FUNCTION')
+        print('Unknown Instruction "' + exec_ + '"')
+
+while code:
+    line = code.pop().lstrip()
+    if line.startswith('//') \
+    or line.isspace() \
+    or line == '': 
+        continue
+    exec_ = line.upper().split(' ')
+    do_code(exec_)
